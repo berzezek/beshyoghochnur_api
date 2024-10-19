b7687a9781c04b5b9f6350c66f85776c339ba43c
berzezek <wknduz@gmail.com>
2024-10-19 14:50:31 +0500

    fix detail not found

--- catalog/api/views.py
+++ catalog/api/views.py
@@ -1,8 +1,12 @@
 from django.db.models import Q, Case, When, Value, IntegerField
 from rest_framework import viewsets
+from rest_framework.exceptions import NotFound
+
 from rest_framework.permissions import IsAuthenticatedOrReadOnly
+from rest_framework.response import Response
 from catalog.api.serializers import ProductSerializer, CategorySerializer
 from catalog.models import Product, Category
+
 
 class ProductViewSet(viewsets.ModelViewSet):
     queryset = Product.objects.filter(is_active=True)
@@ -16,16 +20,34 @@ class ProductViewSet(viewsets.ModelViewSet):
         queryset = Product.objects.filter(is_active=True).language(lang)
 
         if search_query:
-            queryset = queryset.filter(
-                Q(translations__name__icontains=search_query) |
-                Q(translations__description__icontains=search_query)
-            ).annotate(
-                relevance=(
-                    Case(When(translations__name__icontains=search_query, then=Value(2)), default=Value(0), output_field=IntegerField()) +
-                    Case(When(translations__description__icontains=search_query, then=Value(1)), default=Value(0), output_field=IntegerField())
+            queryset = (
+                queryset.filter(
+                    Q(translations__name__icontains=search_query)
+                    | Q(translations__description__icontains=search_query)
                 )
-            ).order_by('-relevance')
-        
+                .annotate(
+                    relevance=(
+                        Case(
+                            When(
+                                translations__name__icontains=search_query,
+                                then=Value(2),
+                            ),
+                            default=Value(0),
+                            output_field=IntegerField(),
+                        )
+                        + Case(
+                            When(
+                                translations__description__icontains=search_query,
+                                then=Value(1),
+                            ),
+                            default=Value(0),
+                            output_field=IntegerField(),
+                        )
+                    )
+                )
+                .order_by("-relevance")
+            )
+
         # Удаление дубликатов по id
         unique_results = []
         seen_ids = set()
@@ -36,7 +58,15 @@ class ProductViewSet(viewsets.ModelViewSet):
 
         return unique_results
 
-
+    def get_object(self):
+        product = (
+            Product.objects.filter(slug=self.kwargs["slug"], is_active=True)
+            .language(self.request.query_params.get("lang", "uz"))
+            .first()
+        )
+        if not product:
+            raise NotFound("Product not found")
+        return product
 
 
 class CategoryViewSet(viewsets.ModelViewSet):
